41e6487433579bd7afa974660f77d805c7e3dae9
Joshua Haupt <joshua@hauptj.com>
2026-01-22 11:59:03 -0600

    Added main test cli arg test driver

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -2,51 +2,22 @@
 // Created by josh on 1/21/26.
 //
 
-#include <git2.h>
 #include <stdio.h>
 #include <string>
+#include "gitrepoutil.cpp"
 
-int main() {
+int main(int argc, char* argv[]) {
 
-    git_repository *repo = nullptr;
-    int error;
+    if (argc > 1) {
+        printf("Number of repos to check: %d\n", argc - 1);
 
-    git_libgit2_init();
-
-    error = git_repository_open(&repo, "/home/josh/dev/hauptj/POC-FS-Scanners-and-GIt-Repo-Remote-Verifiers/.git");
-    if (error < 0) {
-        const git_error *e = git_error_last();
-        printf("Error %d opening repository: %s\n", error, e->message);
-        return error;
-    }
-
-    git_strarray remoteNames = {0};
-    error = git_remote_list(&remoteNames, repo);
-    if (error < 0) {
-        const git_error *e = git_error_last();
-        printf("Error %d getting remotes repository: %s\n", error, e->message);
-        return error;
-    }
-
-    for (int i = 0; i < remoteNames.count; i++) {
-        printf("Remote %d: %s\n", i , remoteNames.strings[i]);
-        git_remote* remote = nullptr;
-        error = git_remote_lookup(&remote, repo, remoteNames.strings[i]);
-        if (error < 0) {
-            const git_error *e = git_error_last();
-            printf("Error %d getting remotes repository urls: %s\n", error, e->message);
-            return error;
+        for (int i = 1; i < argc; i++) {
+            std::string repoPath = argv[i];
+            printf("Checking repository: %s\n", repoPath.c_str());
+            gitrepoutil gitutil = gitrepoutil(repoPath);
+            printf("Git Repo Remote URL is valid?: %s\n", gitutil.getIsValidRemote() ? "Yes" : "No");
         }
-        const char* url = git_remote_url(remote);
-        std::string remoteUrl = url ? url : "";
-        printf("URL: %s\n", remoteUrl.c_str());
-
-
     }
-
-    git_repository_free(repo);
-
-    git_libgit2_shutdown();
 
     return 0;
 }
